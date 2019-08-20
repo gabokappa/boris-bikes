7e5650be541ca17a9ebcd873aafefd4f9ff5599d
Daniel Segall <segalld@gmail.com>
2019-08-20 16:11:32 +0100

    Docking Stations return working bikes

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,13 @@
 require "bike"
 
 describe Bike do
+  let(:bike) { Bike.new }
+
   it "responds to #working?" do
-    expect(Bike.new).to respond_to "working?"
+    expect(bike).to respond_to "working?"
+  end
+
+  it "works by default" do
+    expect(bike.working?).to eq(true)
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,17 @@
 require "docking_station"
 
 describe DockingStation do
+  let(:docking_station) { DockingStation.new }
+
   it "responds to release_bike" do
-    expect(DockingStation.new).to respond_to "release_bike"
+    expect(docking_station).to respond_to "release_bike"
+  end
+
+  it "releases a bike" do
+    expect(docking_station.release_bike).to be_instance_of Bike
+  end
+
+  it "releases a working bike" do
+    expect(docking_station.release_bike.working?).to eq true
   end
 end
